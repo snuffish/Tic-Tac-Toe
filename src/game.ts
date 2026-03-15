08a1c1c13d223d56b2c9fc943ee2adce6755f48d
Snuffish <snuffish90@gmail.com>
2026-03-15 08:15:25 +0100

    wip

--- src/game.ts
+++ src/game.ts
@@ -2,26 +2,53 @@ import p5 from 'p5';
 import { CANVAS_HEIGHT, CANVAS_WIDTH } from './env.ts';
 
 const sketch = (p: p5) => {
-  const container = p.createDiv();
+  let wrapper: p5.Element;
 
   const createSquare = (x: number, y: number, size: number) => {
     const square = p.createDiv();
+    square.parent(wrapper);
+    square.style('position', 'absolute');
     square.position(x, y);
-    square.attribute('row', String(0))
-    square.attribute('col', String(1));
 
-    return square
+    square.style('width', `${size}px`);
+    square.style('height', `${size}px`);
+    square.style('border', '1px solid black');
+    square.style('box-sizing', 'border-box');
+
+    square.mouseOver(() => {
+      square.style('background-color', 'yellow');
+
+      square.html(`${x}x${y}`);
+    })
+
+    square.mouseOut(() => {
+      square.style('background-color', 'transparent');
+
+      square.html('');
+    });
+
+
+    return square;
   };
 
-  const square = createSquare(0, 0, 50)
-  container.child(square)
-
   p.setup = () => {
-    p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    wrapper = p.createDiv();
+    wrapper.style('position', 'relative');
+    wrapper.style('width', `${CANVAS_WIDTH}px`);
+    wrapper.style('height', `${CANVAS_HEIGHT}px`);
+
+    const canvas = p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    canvas.parent(wrapper);
+
+    createSquare(50, 50, 50);
+    createSquare(100 , 50, 50);
+
     p.background(240);
   };
 
-  p.draw = () => {};
+  p.draw = () => {
+    p.background(240);
+  };
 };
 
 export function mountSketch(container?: HTMLElement) {
