188c9332c44b857c9bdd8de78aaba79871322eb6
Christoffer Engman <snuffish90@gmail.com>
2026-03-16 17:39:57 +0100

    wip

--- src/game.ts
+++ src/game.ts
@@ -1,27 +1,102 @@
 import p5 from 'p5';
-import { CANVAS_HEIGHT, CANVAS_WIDTH, magicSquareArray } from './env.ts';
-import { Cell } from './components/cell.ts';
-import { Board } from './components/board.ts';
+
+const SQUARE_SIZE = 50;
+
+const CANVAS_HEIGHT = SQUARE_SIZE * 3;
+const CANVAS_WIDTH = SQUARE_SIZE * 3;
 
 const sketch = (p: p5) => {
-  let cells: Cell[] = [];
-  let board: Board
-
   p.setup = () => {
-    window.currentPlayer = 'player1'
     p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     p.background(240);
+  };
 
-    cells = magicSquareArray.map((nr) => new Cell(p, nr));
+  const color = {
+    red: [255, 0, 0],
+    blue: [0, 0, 255]
+  };
 
-    board = new Board()
-    board.addCell(cells)
+  const drawX = (x: number, y: number) => {
+    p.push();
+    p.translate(x, y);
+
+    const centerOrigin = SQUARE_SIZE / 2;
+    const originOffset = centerOrigin / 2;
+
+    p.stroke(color.red);
+
+    p.line(
+      centerOrigin - originOffset,
+      originOffset,
+      centerOrigin + originOffset,
+      SQUARE_SIZE - originOffset
+    );
+    p.line(
+      SQUARE_SIZE - originOffset,
+      originOffset,
+      originOffset,
+      SQUARE_SIZE - originOffset
+    );
+    p.pop();
+  };
+
+  const drawO = (x: number, y: number) => {
+    p.push();
+    p.translate(x, y);
+
+    p.stroke(color.blue);
+
+    const centerOrigin = SQUARE_SIZE / 2;
+    const diameter = SQUARE_SIZE / 2;
+
+    p.circle(centerOrigin, centerOrigin, diameter);
+    p.pop();
+  };
+
+  let currentPlayer: Player = 'player1';
+
+  const board = [
+    ['X', 'O', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+
+  const nextTurn = () => {
+    currentPlayer = currentPlayer === 'player1' ? 'player2' : 'player1';
+  }
+
+  p.mousePressed = () => {
+    const row = p.floor(p.map(p.mouseY, 0, CANVAS_HEIGHT, 0, 3));
+    const col = p.floor(p.map(p.mouseX, 0, CANVAS_WIDTH, 0, 3));
+
+    if (board[row][col] === '') {
+      const symbol = currentPlayer === 'player1' ? 'X' : 'O';
+      board[row][col] = symbol;
+      nextTurn();
+    }
   };
 
   p.draw = () => {
     p.background(240);
 
-    cells.forEach((cell) => cell.show());
+    board.map((row, rowIndex) => {
+      row.map((cell, cellIndex) => {
+        const cellPos = cellIndex;
+
+        const size = SQUARE_SIZE;
+
+        const x = cellPos * size;
+        const y = rowIndex * size;
+
+        p.square(x, y, size);
+
+        if (cell === 'X') {
+          drawX(x, y);
+        } else if (cell === 'O') {
+          drawO(x, y);
+        }
+      });
+    });
   };
 };
 
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,23 +1,10 @@
-import type { BoxProps } from '../components/box.ts';
-import type { GameProps } from '../game.ts';
-
 declare module '*.css';
 
 declare global {
-  interface Window {
-    currentPlayer: Player
-  }
-
   type RGB = [number, number, number];
   type Color = 'NONE' | 'RED' | 'GREEN' | 'BLUE';
 
   type Player = 'player1' | 'player2';
-
-  interface GameObject {
-    onDraw: () => void;
-    onUpdate?: () => void;
-    onMousePressed?: () => void;
-  }
 }
 
 export {};
